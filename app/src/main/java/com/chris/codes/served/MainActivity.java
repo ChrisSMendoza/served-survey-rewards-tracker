59506768e19d460eca4ee8989524a0b224d11236
mendoza1 <csmendoza@ucdavis.edu>
2019-02-14 09:38:14 -0800

    RewardViewModel in mainActivity used to observe all rewards; db data added on creation

--- app/src/main/java/com/chris/codes/served/MainActivity.java
+++ app/src/main/java/com/chris/codes/served/MainActivity.java
@@ -1,6 +1,9 @@
 package com.chris.codes.served;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,9 +11,15 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
+
+    private RewardViewModel mRewardViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+
+        mRewardViewModel = ViewModelProviders.of(this).get(RewardViewModel.class);
+
+        mRewardViewModel.getAllRewards().observe(this, new Observer<List<Reward>>() {
+            @Override
+            public void onChanged(@Nullable final List<Reward> rewards) {
+
+                adapter.setRewards(rewards); // update the cached copy of the rewards in the adapter
+            }
+        });
     }
 
     public void goToTakeSurvey(View view) {
--- app/src/main/java/com/chris/codes/served/RewardRoomDatabase.java
+++ app/src/main/java/com/chris/codes/served/RewardRoomDatabase.java
@@ -1,9 +1,12 @@
 package com.chris.codes.served;
 
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
 // database only holds rewards
 @Database(entities = {Reward.class}, version = 1)
@@ -13,6 +16,37 @@ public abstract class RewardRoomDatabase extends RoomDatabase {
 
     // singleton pattern to prevent multiple instances of the database
     private static volatile RewardRoomDatabase INSTANCE;
+
+    private static class PopulateDBAsync extends AsyncTask<Void, Void, Void> {
+
+        private final RewardDao mDao;
+
+        PopulateDBAsync(RewardRoomDatabase db) {
+            mDao = db.rewardDao();
+        }
+        @Override
+        protected Void doInBackground(final Void... params) {
+            mDao.deleteAll();
+
+            Reward reward = new Reward("7eleven-code");
+            mDao.insert(reward);
+            reward = new Reward("popeyes-code");
+            mDao.insert(reward);
+
+            return null;
+        }
+
+    }
+
+    private static RoomDatabase.Callback sRewardDatabaseCallback =
+            new RoomDatabase.Callback() {
+
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            new PopulateDBAsync(INSTANCE).execute();
+        }
+    };
 
     static RewardRoomDatabase getDatabase(final Context context) {
 
@@ -24,6 +58,7 @@ public abstract class RewardRoomDatabase extends RoomDatabase {
                     // create database in the app context
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             RewardRoomDatabase.class, "reward_database")
+                            .addCallback(sRewardDatabaseCallback)
                             .build();
                 }
             }
