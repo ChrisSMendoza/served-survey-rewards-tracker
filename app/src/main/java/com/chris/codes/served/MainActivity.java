7caa977cd9c59fccbdbfe81c253e36506b0903e8
mendoza1 <csmendoza@ucdavis.edu>
2019-02-17 21:09:42 -0800

    RewardsAvailable created, moved RecyclerView to RewardsAvailable, RewardsAvailable has stubs for functions in master branch

--- app/src/main/java/com/chris/codes/served/MainActivity.java
+++ app/src/main/java/com/chris/codes/served/MainActivity.java
@@ -1,47 +1,22 @@
 package com.chris.codes.served;
 
-import android.arch.lifecycle.Observer;
-
-import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
 
-    private RewardViewModel mRewardViewModel;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-        final RewardListAdapter adapter = new RewardListAdapter(this);
-
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        mRewardViewModel = ViewModelProviders.of(this).get(RewardViewModel.class);
-
-        mRewardViewModel.getAllRewards().observe(this, new Observer<List<Reward>>() {
-            @Override
-            public void onChanged(@Nullable final List<Reward> rewards) {
-
-                adapter.setRewards(rewards); // update the cached copy of the rewards in the adapter
-            }
-        });
     }
 
     public void goToTakeSurvey(View view) {
